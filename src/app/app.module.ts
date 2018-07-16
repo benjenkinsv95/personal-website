c83547570b676d1bc5761177d7657490d3c970ed
Ben Jenkins <benjenkinsv95@gmail.com>
2018-07-15 21:05:45 -0400

    Bug fix, robokind leftover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import {NavbarComponent} from './shared/navbar/navbar.component';
 import {FooterComponent} from './shared/footer/footer.component';
 
 import {HomeModule} from './home/home.module';
-import { RobokindComponent } from './robokind/robokind.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
